7d83764e8a47cae31f092f581974fd8c6b6ec0b7
Nezquik_lap <you@example.com>
2022-05-03 16:53:33 +0500

    refactoring

--- src/main/kotlin/com/nzqk/challengerbot/ChallengerBot.kt
+++ src/main/kotlin/com/nzqk/challengerbot/ChallengerBot.kt
@@ -11,140 +11,136 @@ import com.github.kotlintelegrambot.network.fold
 import com.nzqk.challengerbot.command.CreateTaskCommand
 import com.nzqk.challengerbot.command.GetMyTasksCommand
 import com.nzqk.challengerbot.command.GetTaskCommand
-import com.nzqk.challengerbot.observer.notificator.TaskNotificator
 import com.nzqk.challengerbot.observer.CacheMessage
 import com.nzqk.challengerbot.observer.MessageObserver
-import org.springframework.beans.factory.annotation.Autowired
+import com.nzqk.challengerbot.observer.notificator.TaskNotificator
 import org.springframework.stereotype.Component
 
 @Component
-object ChallengerBot {
+class ChallengerBot(
+
+    val createTaskCommand: CreateTaskCommand,
+
+    val getMyTasksCommand: GetMyTasksCommand,
+
+    val getTaskCommand: GetTaskCommand,
+
+    val observer: MessageObserver,
+
+    val notificator: TaskNotificator
+) {
 
     lateinit var bot: Bot
 
-    @Autowired
-    private lateinit var createTaskCommand: CreateTaskCommand
-
-    @Autowired
-    private lateinit var getMyTasksCommand: GetMyTasksCommand
-
-    @Autowired
-    private lateinit var getTaskCommand: GetTaskCommand
-
-    @Autowired
-    private lateinit var observer: MessageObserver
-
-    @Autowired
-    private lateinit var notificator: TaskNotificator
-
     init {
-        bot = bot {
+        bot =
+            bot {
+                token = "5393101475:AAG4uNHFuKBTO_hNKRCcBOHct3QF9EvQWEE"
+                dispatch {
 
-            token = "5393101475:AAG4uNHFuKBTO_hNKRCcBOHct3QF9EvQWEE"
-            dispatch {
-
-                command("help") {
-                    bot.sendMessage(
-                        chatId = ChatId.fromId(message.chat.id),
-                        text = " Возможные команды",
-                        replyToMessageId = update.message!!.messageId,
-                        replyMarkup = KeyboardReplyMarkup.createSimpleKeyboard(
-                            listOf(
-                                listOf(CommandMessage.CREATE_TASK.message),
-                                listOf(CommandMessage.MY_TASK.message),
-                                listOf(CommandMessage.GET_TASK.message)
+                    command("commands") {
+                        bot.sendMessage(
+                            chatId = ChatId.fromId(message.chat.id),
+                            replyToMessageId = update.message!!.messageId,
+                            replyMarkup = KeyboardReplyMarkup.createSimpleKeyboard(
+                                listOf(
+                                    listOf(CommandMessage.CREATE_TASK.message),
+                                    listOf(CommandMessage.MY_TASK.message),
+                                    listOf(CommandMessage.GET_TASK.message)
+                                ),
+                                selective = true
                             )
                         )
-                    )
-                }
+                    }
 
-                message {
-                    if (message.text == CommandMessage.MY_TASK.message) {
-                        val tasks = getMyTasksCommand.execute(message).let {
-                            it.joinToString("\n") { task ->
-                                task.toShortString()
+                    message {
+                        if (message.text == CommandMessage.MY_TASK.message) {
+                            val tasks = getMyTasksCommand.execute(message).let {
+                                it.joinToString("\n") { task ->
+                                    task.toShortString()
+                                }
                             }
+                            bot.sendMyMessage(message, tasks)
                         }
-                        bot.sendMyMessage(message, tasks)
+
+                        if (message.text == CommandMessage.GET_TASK.message) {
+                            bot.sendMyMessageWithObserver(message, CommandMessage.GET_TASK_NUM.message)
+                        }
+
+                        if (message.text == CommandMessage.CREATE_TASK.message) {
+                            bot.sendMyMessageWithObserver(message, CommandMessage.ADD_TITLE.message)
+                        }
                     }
 
-                    if (message.text == CommandMessage.GET_TASK.message) {
-                        bot.sendMyMessageWithObserver(message, CommandMessage.GET_TASK_NUM.message)
-                    }
+                    message(Filter.Reply) {
 
-                    if (message.text == CommandMessage.CREATE_TASK.message) {
-                        bot.sendMyMessageWithObserver(message, CommandMessage.ADD_TITLE.message)
-                    }
-                }
-
-                message(Filter.Reply) {
-
-                    if (message.replyToMessage?.text == CommandMessage.GET_TASK_NUM.message) {
-                        message.findCacheMessage()?.also { cacheMessage ->
-                            if (message.checkCorrectUser(cacheMessage)) {
-                                getTaskCommand.execute(message).also {
-                                    observer.remove(message.replyToMessage!!.messageId)
-                                    bot.sendMyMessage(message, it.toString(), null)
+                        if (message.replyToMessage?.text == CommandMessage.GET_TASK_NUM.message) {
+                            message.findCacheMessage()?.also { cacheMessage ->
+                                if (message.checkCorrectUser(cacheMessage)) {
+                                    getTaskCommand.execute(message).also {
+                                        observer.remove(message.replyToMessage!!.messageId)
+                                        bot.sendMyMessage(message, it.toString(), null)
+                                    }
                                 }
                             }
                         }
-                    }
 
-                    if (message.replyToMessage?.text == CommandMessage.ADD_TITLE.message) {
-                        message.findCacheMessage()?.also { cacheMessage ->
-                            if (message.checkCorrectUser(cacheMessage)) {
-                                cacheMessage.createdTime = System.currentTimeMillis()
-                                cacheMessage.task.title = message.text
-                                cacheMessage.task.chat = message.chat.id
-                                observer.remove(message.replyToMessage!!.messageId)
+                        if (message.replyToMessage?.text == CommandMessage.ADD_TITLE.message) {
+                            message.findCacheMessage()?.also { cacheMessage ->
+                                if (message.checkCorrectUser(cacheMessage)) {
+                                    cacheMessage.createdTime = System.currentTimeMillis()
+                                    cacheMessage.task.title = message.text
+                                    cacheMessage.task.chat = message.chat.id
+                                    observer!!.remove(message.replyToMessage!!.messageId)
 
-                                bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DESCRIPTION.message, cacheMessage)
+                                    bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DESCRIPTION.message, cacheMessage)
+                                }
                             }
                         }
-                    }
 
-                    if (message.replyToMessage?.text == CommandMessage.ADD_DESCRIPTION.message) {
-                        message.findCacheMessage()?.also { cacheMessage ->
-                            if (message.checkCorrectUser(cacheMessage)) {
-                                cacheMessage.createdTime = System.currentTimeMillis()
-                                cacheMessage.task.description = message.text
+                        if (message.replyToMessage?.text == CommandMessage.ADD_DESCRIPTION.message) {
+                            message.findCacheMessage()?.also { cacheMessage ->
+                                if (message.checkCorrectUser(cacheMessage)) {
+                                    cacheMessage.createdTime = System.currentTimeMillis()
+                                    cacheMessage.task.description = message.text
 
-                                observer.remove(message.replyToMessage!!.messageId)
+                                    observer.remove(message.replyToMessage!!.messageId)
 
-                                bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DEADLINE.message, cacheMessage)
+                                    bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DEADLINE.message, cacheMessage)
+                                }
                             }
                         }
-                    }
 
-                    if (message.replyToMessage?.text == CommandMessage.ADD_DEADLINE.message ||
-                        message.replyToMessage?.text == CommandMessage.ADD_DEADLINE_WRONG.message
-                    ) {
-                        message.findCacheMessage()?.also { cacheMessage ->
-                            if (message.checkCorrectUser(cacheMessage)) {
-                                cacheMessage.createdTime = System.currentTimeMillis()
+                        if (message.replyToMessage?.text == CommandMessage.ADD_DEADLINE.message ||
+                            message.replyToMessage?.text == CommandMessage.ADD_DEADLINE_WRONG.message
+                        ) {
+                            message.findCacheMessage()?.also { cacheMessage ->
+                                if (message.checkCorrectUser(cacheMessage)) {
+                                    cacheMessage.createdTime = System.currentTimeMillis()
 
-                                val days = message.text!!.trim().toIntOrNull()
+                                    val days = message.text!!.trim().toIntOrNull()
 
-                                if (days == null) {
-                                    observer.remove(message.replyToMessage!!.messageId)
-                                    bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DEADLINE_WRONG.message, cacheMessage)
-                                } else {
-                                    cacheMessage.task.deadlinePlusHours(days.toLong())
-                                    val task = createTaskCommand.execute(message, cacheMessage.task)
-                                    observer.remove(message.replyToMessage!!.messageId)
+                                    if (days == null) {
+                                        observer.remove(message.replyToMessage!!.messageId)
+                                        bot.sendMyMessageWithObserver(message, CommandMessage.ADD_DEADLINE_WRONG.message, cacheMessage)
+                                    } else {
+                                        cacheMessage.task.deadlinePlusHours(days.toLong())
+                                        val task = createTaskCommand.execute(message, cacheMessage.task)
+                                        observer.remove(message.replyToMessage!!.messageId)
 
-                                    bot.sendMyMessage(message, "Задача с номером ${task.id} успешно создана!", null)
+                                        bot.sendMyMessage(message, "Задача с номером ${task.id} успешно создана!", null)
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
-        }
+        bot.setMyCommands(listOf(BotCommand("commands", "Все команды")))
         bot.startPolling()
     }
 
-    private fun Message.findCacheMessage() = observer.get(replyToMessage!!.messageId)
+    private fun Message.findCacheMessage() = observer!!.get(replyToMessage!!.messageId)
 
     private fun Message.checkCorrectUser(cacheMessage: CacheMessage) = cacheMessage.chatId == chat.id && cacheMessage.userId == from?.id
 
@@ -165,13 +161,13 @@ object ChallengerBot {
             it.fold({ resp ->
                 val result = resp!!.result!!
                 if (cacheMessage == null) {
-                    observer.add(
+                    observer!!.add(
                         messageId = result.messageId,
                         chatId = result.chat.id,
                         userId = result.replyToMessage!!.from!!.id,
                     )
                 } else {
-                    observer.add(
+                    observer!!.add(
                         messageId = result.messageId,
                         cacheMessage = cacheMessage
                     )
--- src/main/kotlin/com/nzqk/challengerbot/observer/notificator/TaskNotificator.kt
+++ src/main/kotlin/com/nzqk/challengerbot/observer/notificator/TaskNotificator.kt
@@ -6,7 +6,9 @@ import com.nzqk.challengerbot.ChallengerBot
 import com.nzqk.challengerbot.model.TaskStatus
 import com.nzqk.challengerbot.observer.JobTimer
 import com.nzqk.challengerbot.service.TaskService
+import com.nzqk.challengerbot.utils.ApplicationContextUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Component
 import kotlin.time.Duration.Companion.minutes
 import kotlin.time.DurationUnit
@@ -17,19 +19,23 @@ class TaskNotificator : JobTimer(1L.minutes.toLong(DurationUnit.MILLISECONDS)) {
     @Autowired
     private lateinit var taskService: TaskService
 
+    val appCtx: ApplicationContext by lazy { ApplicationContextUtils.applicationContext!! }
+    val challengerBot by lazy { appCtx.getBean("challengerBot", ChallengerBot::class.java) }
+
     override fun job() {
         val tasks = taskService.getAllTaskWithDeadline()
 
         tasks.forEach { task ->
             task.status = TaskStatus.NOTIFIED
-            ChallengerBot.bot.sendMessage(
+            challengerBot.bot.sendMessage(
                 chatId = ChatId.fromId(task.chat!!),
-                text = notification() + task.toString(),
+                text = notification(task.owner!!.id!!, task.owner!!.name!!) + task.toString(),
                 parseMode = ParseMode.HTML,
             )
             taskService.addTask(task)
         }
     }
 
-    private fun notification() = "<i>ЗАКОНЧИЛСЯ СРОК ПО ЗАДАЧЕ</i>\n\n"
+    private fun notification(id: Long, name: String) = "<a href=\"tg://user?id=$id\">$name</a>" +
+        " \n<i>ЗАКОНЧИЛСЯ СРОК ПО ЗАДАЧЕ</i>\n\n"
 }
